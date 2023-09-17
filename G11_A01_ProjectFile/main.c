ef1ec5445b03fedd4f9e2085deed0563069f7c28
AdityaShirodkar-IITDh <ee23dp001@iitdh.ac.in>
2023-09-17 10:37:04 +0530

    Read-Modify-Write on Port F

--- G11_A01_ProjectFile/main.c
+++ G11_A01_ProjectFile/main.c
@@ -4,18 +4,18 @@
 
 int main(void)
 {
-    SYSCTL_RCGCGPIO_R |= 0x02;  // enable clock to GPIOB
-    GPIO_PORTB_DIR_R |= 0x02;   // set PB1 as output
-    GPIO_PORTB_DEN_R |= 0x02;   // enable digital function for PB1
+    SYSCTL_RCGCGPIO_R |= 0x20;	// enable clock to GPIOF
+    GPIO_PORTF_DIR_R |= 0x02;	// set PF1 as output
+    GPIO_PORTF_DEN_R |= 0x02;	// enable digital function for PF1
+    
     int i,x;
     x=SYSCTL_RCC_R;
+    
     while(1)
     {
-       GPIO_PORTB_DATA_R = 0x02;   // toggle PB1
-       // delay for 1 millisecond
-       for(i = 0; i <800; i++){}
-       // delay for 1 millisecond
-       GPIO_PORTB_DATA_R= 0x00;     // toggle PB1
-       for(i = 0; i <800; i++){}
+        GPIO_PORTF_DATA_R = 0x02; 	// Turn On PortF LED
+       for(i = 0; i <800; i++){}	// delay for 1 millisecond
+           GPIO_PORTF_DATA_R= 0x00;	// Turn Off PortF LED
+       for(i = 0; i <800; i++){}	// delay for 1 millisecond
     }
 }
